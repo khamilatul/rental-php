ac69e6e75dc761604fdf41c8fa864acaa6de2e2f
Khamilatul <khamilatul.zahro999@gmail.com>
2017-10-23 18:47:03 +0700

    delete table service

--- modules/service/service-index.php
+++ modules/service/service-index.php
@@ -39,9 +39,9 @@
               <td><?php echo $r['jenis_service'] ?></td>
               <td>
                   <div class="small button-group">
-                      <a href="?module=service-show?id=<?php echo $r['id']; ?>" class=" button">View</a>
-                      <a href="?module=service-edit?id=<?php echo $r['id']; ?>" class="secondary button">Edit</a>
-                      <a href="?module=service-delete?id=<?php echo $r['id']; ?>"onClick='return confirm("Apakah yakin menghapus?")' class="alert button">Delete</a>
+                      <a href="?module=service-show&id=<?php echo $r['id']; ?>" class=" button">View</a>
+                      <a href="?module=service-edit&id=<?php echo $r['id']; ?>" class="secondary button">Edit</a>
+                      <a href="?module=service-delete&id=<?php echo $r['id']; ?>"onClick='return confirm("Apakah yakin menghapus?")' class="alert button">Delete</a>
                   </div>
               </td>
           </tr>
